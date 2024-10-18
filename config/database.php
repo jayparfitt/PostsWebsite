369c531a344d91db1e6c73a952a829f73404cfc3
Jay <parfitt2001@gmail.com>
2024-10-18 12:28:35 +0100

    Created Admins table for admins users

--- config/database.php
+++ config/database.php
@@ -32,6 +32,7 @@ return [
     'connections' => [
 
         'sqlite' => [
+            
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
